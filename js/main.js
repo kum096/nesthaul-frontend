fad05f0708eb641dd466187fe9ed710370e21d27
KUM <dollarkum9@gmail.com>
2025-06-27 20:11:48 +0100

    ok1

--- js/main.js
+++ js/main.js
@@ -100,3 +100,16 @@ document.querySelectorAll(".nav-links a").forEach(link => {
       alert('An error occurred: ' + error.message);
     }
   });
+
+
+  //online chat
+
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/685ee829356b5f1912cd293a/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
